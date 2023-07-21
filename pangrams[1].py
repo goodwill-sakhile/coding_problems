4d50a3eaf6e29a2be14a564e6b713d1107af974a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-07-21 09:20:22 +0200

    panagram code solution

--- pangrams[1].py
+++ pangrams[1].py
@@ -1,2 +1,9 @@
 def solvePangrams(_string):
-	pass
+	alphabet = "abcdefghijjklmnopqrstuvwxyz"
+	for element in _string:
+		if element in alphabet:
+			alphabet = alphabet[0:alphabet.find(element)] + alphabet[alphabet.find(lement) + 1:]
+	if alphabet == "":
+		return "Panagram"
+	else:
+		return "Not Panagram"
