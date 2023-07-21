ef217375d56be9ca65bfb8db3dd2d10ae69d07bd
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-07-21 09:19:20 +0200

    count sort algorithm solution

--- countSort[1].py
+++ countSort[1].py
@@ -1,2 +1,9 @@
 def solveCountSort(_array):
-	pass
+	max_value = max(_array)
+	zeros_array = []
+	for i in range(max_value + 1):
+		zeros_array.append(0)
+	for index in _array:
+		zeros_array[index] = zeros_array[index] + 1
+	return zeros_array
+print(solveCountSort([1, 1, 3, 2, 1]))
