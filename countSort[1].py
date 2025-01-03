1bf4492fc6a872cdb3b6c6b572666a1339b7ee64
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-01-03 13:17:47 -0800

    Delete countSort[1].py

--- countSort[1].py
+++
@@ -1,9 +0,0 @@
-def solveCountSort(_array):
-	max_value = max(_array)
-	zeros_array = []
-	for i in range(max_value + 1):
-		zeros_array.append(0)
-	for index in _array:
-		zeros_array[index] = zeros_array[index] + 1
-	return zeros_array
-print(solveCountSort([1, 1, 3, 2, 1]))
